2ca8f4d9435f224b2d5f21412a656ed1a400b406
Scott <dscottwiggins@gmail.com>
2021-10-15 22:59:17 -0400

    set up releases and fly.io deployment stuff

--- config/runtime.exs
+++ config/runtime.exs
@@ -14,10 +14,14 @@ if config_env() == :prod do
       For example: ecto://USER:PASS@HOST/DATABASE
       """
 
+  app_name =
+    System.get_env("FLY_APP_NAME") ||
+      raise "FLY_APP_NAME not available"
+
   config :retroflect, Retroflect.Repo,
     # ssl: true,
-    # socket_options: [:inet6],
     url: database_url,
+    socket_options: [:inet6],
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
@@ -33,13 +37,16 @@ if config_env() == :prod do
       """
 
   config :retroflect, RetroflectWeb.Endpoint,
+    server: true,
+    url: [host: "#{app_name}.fly.dev", port: 80],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
+      port: String.to_integer(System.get_env("PORT") || "4000"),
+      transport_options: [socket_opts: [:inet6]]
     ],
     secret_key_base: secret_key_base
 
