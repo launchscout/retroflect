04cdcea4e2ee337c35153ac15865c654eec66c8c
Andrew Moore <andrew@lessthanseventy.com>
2021-10-17 14:33:22 -0600

    fix broken test after moving root route

--- test/retroflect_web/controllers/page_controller_test.exs
+++ test/retroflect_web/controllers/page_controller_test.exs
@@ -1,6 +1,8 @@
 defmodule RetroflectWeb.PageControllerTest do
   use RetroflectWeb.ConnCase
 
+  setup :register_and_log_in_user
+
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
     assert html_response(conn, 200)
