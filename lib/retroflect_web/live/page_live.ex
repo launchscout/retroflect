cc33e3e8aa8664f050decfbc54b7e4266aabd1f0
Andrew Moore <andrew@lessthanseventy.com>
2021-10-17 14:39:32 -0600

    address credo warnings

--- lib/retroflect_web/live/page_live.ex
+++ lib/retroflect_web/live/page_live.ex
@@ -2,6 +2,9 @@ defmodule RetroflectWeb.PageLive do
   use RetroflectWeb, :live_view
   on_mount RetroflectWeb.UserLiveAuth
 
+  @moduledoc """
+  Root live view
+  """
   def mount(_params, session, socket) do
     {:ok, socket}
   end
--- lib/retroflect_web/user_live_auth.ex
+++ lib/retroflect_web/user_live_auth.ex
@@ -3,12 +3,20 @@ defmodule RetroflectWeb.UserLiveAuth do
 
   alias Retroflect.Accounts
 
+  @moduledoc """
+  Module to add current user to session, or redirect to /users/log_in if not logged in
+  """
+
   def mount(params, %{"user_token" => user_token} = _session, socket) do
     socket =
       assign_new(socket, :current_user, fn ->
         Accounts.get_user_by_session_token(user_token)
       end)
 
-    {:cont, socket}
+    if socket.assigns.current_user do
+      {:cont, socket}
+    else
+      {:halt, redirect(socket, to: "/users/log_in")}
+    end
   end
 end
